e16f443f69f968cd54bf7912b02d340198ee7344
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-18 22:21:55 -0500

    add code for profile generator

--- survey.js
+++ survey.js
@@ -5,8 +5,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+let answers = [];
 
-  rl.close();
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+  answers.push(name);
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+    answers.push(activity);
+    rl.question('What do you listen to while doing that? ', (listen) => {
+      answers.push(listen);
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (meal) => {
+        answers.push(meal);
+        rl.question('What\'s your favourite thing to eat for that meal? ', (dish) => {
+          answers.push(dish);
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            answers.push(sport);
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              answers.push(superpower);
+              console.log(`My name is ${name}. I like ${activity.toLowerCase()} and I like to listen to ${listen.toLowerCase()} while doing so. My favourite meal of the day is ${meal.toLowerCase()}. My favourite thing to eat during ${meal.toLowerCase()} is ${dish.toLowerCase()}. My absolute favourite sport is ${sport.toLowerCase()}. My superpower is ${superpower.toLowerCase()}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
+
+
+
+// rl.close();
