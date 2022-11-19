39fd0d634f75d0906fc11c6b5b590070ecd9ee27
Aarij Anwer <aarij.anwer@gmail.com>
2022-11-18 21:56:54 -0500

    updated code that works for stdin/stdout

--- survey.js
+++ survey.js
@@ -1,10 +1,11 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+const readline = require('readline');
 
-const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
   console.log(`Thank you for your valuable feedback: ${answer}`);
 
   rl.close();
